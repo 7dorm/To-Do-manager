05f3e667a879bb143dcdd4f4cd2c224ec35f1705
Artemiy Dunaev <a.dunaev@g.nsu.ru>
2025-09-24 21:17:18 +0700

    renewed

--- src/main/java/ru/nsu/dunaev/interfaces/Analyzer.java
+++
@@ -1,9 +0,0 @@
-package ru.nsu.dunaev.interfaces;
-
-import ru.nsu.dunaev.Task;
-
-import java.util.List;
-
-public interface Analyzer<T, ID> {
-    T analyze(List<Task<ID>> tasks);
-}
--- src/main/java/ru/nsu/dunaev/interfaces/OutputHandler.java
+++
@@ -1,5 +0,0 @@
-package ru.nsu.dunaev.interfaces;
-
-public interface OutputHandler {
-    void print(String message);
-}
